94e3c34de90070993108b559c8bcba26396ec5d5
Taras Kulchytskyi <tkulc@softserveinc.com>
2020-05-03 20:19:24 +0300

    initial

---
+++ index.js
@@ -0,0 +1,411 @@
+import EARTH from 'assets/flags/EARTH.svg'
+import AF from 'assets/flags/AF.svg'
+import AL from 'assets/flags/AL.svg'
+import DZ from 'assets/flags/DZ.svg'
+import AS from 'assets/flags/AS.svg'
+import AD from 'assets/flags/AD.svg'
+import AO from 'assets/flags/AO.svg'
+import AI from 'assets/flags/AI.svg'
+import AG from 'assets/flags/AG.svg'
+import AR from 'assets/flags/AR.svg'
+import AM from 'assets/flags/AM.svg'
+import AW from 'assets/flags/AW.svg'
+import AU from 'assets/flags/AU.svg'
+import AT from 'assets/flags/AT.svg'
+import AZ from 'assets/flags/AZ.svg'
+import BS from 'assets/flags/BS.svg'
+import BH from 'assets/flags/BH.svg'
+import BD from 'assets/flags/BD.svg'
+import BB from 'assets/flags/BB.svg'
+import BY from 'assets/flags/BY.svg'
+import BE from 'assets/flags/BE.svg'
+import BZ from 'assets/flags/BZ.svg'
+import BJ from 'assets/flags/BJ.svg'
+import BM from 'assets/flags/BM.svg'
+import BT from 'assets/flags/BT.svg'
+import BO from 'assets/flags/BO.svg'
+import BQ from 'assets/flags/BQ.svg'
+import BA from 'assets/flags/BA.svg'
+import BW from 'assets/flags/BW.svg'
+import BR from 'assets/flags/BR.svg'
+import IO from 'assets/flags/IO.svg'
+import BG from 'assets/flags/BG.svg'
+import BF from 'assets/flags/BF.svg'
+import BI from 'assets/flags/BI.svg'
+import KH from 'assets/flags/KH.svg'
+import CN from 'assets/flags/CN.svg'
+import CM from 'assets/flags/CM.svg'
+import CA from 'assets/flags/CA.svg'
+import KY from 'assets/flags/KY.svg'
+import CF from 'assets/flags/CF.svg'
+import TD from 'assets/flags/TD.svg'
+import CL from 'assets/flags/CL.svg'
+import CX from 'assets/flags/CX.svg'
+import CO from 'assets/flags/CO.svg'
+import KM from 'assets/flags/KM.svg'
+import CG from 'assets/flags/CG.svg'
+import CR from 'assets/flags/CR.svg'
+import HR from 'assets/flags/HR.svg'
+import CU from 'assets/flags/CU.svg'
+import CW from 'assets/flags/CW.svg'
+import CY from 'assets/flags/CY.svg'
+import CZ from 'assets/flags/CZ.svg'
+import DK from 'assets/flags/DK.svg'
+import DJ from 'assets/flags/DJ.svg'
+import DM from 'assets/flags/DM.svg'
+import DO from 'assets/flags/DO.svg'
+import EC from 'assets/flags/EC.svg'
+import EG from 'assets/flags/EG.svg'
+import SV from 'assets/flags/SV.svg'
+import GQ from 'assets/flags/GQ.svg'
+import ER from 'assets/flags/ER.svg'
+import EE from 'assets/flags/EE.svg'
+import ET from 'assets/flags/ET.svg'
+import FJ from 'assets/flags/FJ.svg'
+import FI from 'assets/flags/FI.svg'
+import FR from 'assets/flags/FR.svg'
+import GA from 'assets/flags/GA.svg'
+import GM from 'assets/flags/GM.svg'
+import GE from 'assets/flags/GE.svg'
+import DE from 'assets/flags/DE.svg'
+import GH from 'assets/flags/GH.svg'
+import GI from 'assets/flags/GI.svg'
+import GR from 'assets/flags/GR.svg'
+import GL from 'assets/flags/GL.svg'
+import GD from 'assets/flags/GD.svg'
+import GU from 'assets/flags/GU.svg'
+import GT from 'assets/flags/GT.svg'
+import GG from 'assets/flags/GG.svg'
+import GN from 'assets/flags/GN.svg'
+import GW from 'assets/flags/GW.svg'
+import HT from 'assets/flags/HT.svg'
+import HN from 'assets/flags/HN.svg'
+import HK from 'assets/flags/HK.svg'
+import HU from 'assets/flags/HU.svg'
+import IS from 'assets/flags/IS.svg'
+import IN from 'assets/flags/IN.svg'
+import ID from 'assets/flags/ID.svg'
+import IR from 'assets/flags/IR.svg'
+import IQ from 'assets/flags/IQ.svg'
+import IE from 'assets/flags/IE.svg'
+import IM from 'assets/flags/IM.svg'
+import IL from 'assets/flags/IL.svg'
+import IT from 'assets/flags/IT.svg'
+import JM from 'assets/flags/JM.svg'
+import JP from 'assets/flags/JP.svg'
+import JE from 'assets/flags/JE.svg'
+import JQ from 'assets/flags/JQ.svg'
+import KZ from 'assets/flags/KZ.svg'
+import KE from 'assets/flags/KE.svg'
+import KI from 'assets/flags/KI.svg'
+import KP from 'assets/flags/KP.svg'
+import KR from 'assets/flags/KR.svg'
+import KW from 'assets/flags/KW.svg'
+import KG from 'assets/flags/KG.svg'
+import LV from 'assets/flags/LV.svg'
+import LB from 'assets/flags/LB.svg'
+import LS from 'assets/flags/LS.svg'
+import LR from 'assets/flags/LR.svg'
+import LY from 'assets/flags/LY.svg'
+import LI from 'assets/flags/LI.svg'
+import LT from 'assets/flags/LI.svg'
+import LU from 'assets/flags/LU.svg'
+import MO from 'assets/flags/MO.svg'
+import MG from 'assets/flags/MG.svg'
+import MW from 'assets/flags/MW.svg'
+import MY from 'assets/flags/MY.svg'
+import MV from 'assets/flags/MV.svg'
+import ML from 'assets/flags/ML.svg'
+import MT from 'assets/flags/MT.svg'
+import MQ from 'assets/flags/MQ.svg'
+import MR from 'assets/flags/MR.svg'
+import MU from 'assets/flags/MR.svg'
+import MX from 'assets/flags/MX.svg'
+import MD from 'assets/flags/MX.svg'
+import MC from 'assets/flags/MC.svg'
+import MN from 'assets/flags/MN.svg'
+import ME from 'assets/flags/ME.svg'
+import MS from 'assets/flags/MS.svg'
+import MA from 'assets/flags/MA.svg'
+import MZ from 'assets/flags/MZ.svg'
+import MM from 'assets/flags/MM.svg'
+import NA from 'assets/flags/NA.svg'
+import NR from 'assets/flags/NR.svg'
+import NP from 'assets/flags/NP.svg'
+import NL from 'assets/flags/NL.svg'
+import NZ from 'assets/flags/NZ.svg'
+import NI from 'assets/flags/NI.svg'
+import NE from 'assets/flags/NE.svg'
+import NG from 'assets/flags/NG.svg'
+import NU from 'assets/flags/NU.svg'
+import NF from 'assets/flags/NF.svg'
+import MP from 'assets/flags/MP.svg'
+import NO from 'assets/flags/NO.svg'
+import OM from 'assets/flags/OM.svg'
+import PK from 'assets/flags/PK.svg'
+import PW from 'assets/flags/PW.svg'
+import PA from 'assets/flags/PA.svg'
+import PG from 'assets/flags/PG.svg'
+import PY from 'assets/flags/PY.svg'
+import PE from 'assets/flags/PE.svg'
+import PH from 'assets/flags/PH.svg'
+import PN from 'assets/flags/PN.svg'
+import PL from 'assets/flags/PL.svg'
+import PT from 'assets/flags/PT.svg'
+import PR from 'assets/flags/PR.svg'
+import QA from 'assets/flags/QA.svg'
+import MK from 'assets/flags/MK.svg'
+import RO from 'assets/flags/RO.svg'
+import RU from 'assets/flags/RU.svg'
+import RW from 'assets/flags/RW.svg'
+import KN from 'assets/flags/KN.svg'
+import LC from 'assets/flags/LC.svg'
+import VC from 'assets/flags/VC.svg'
+import ST from 'assets/flags/ST.svg'
+import SN from 'assets/flags/SN.svg'
+import SL from 'assets/flags/SL.svg'
+import SK from 'assets/flags/SK.svg'
+import SI from 'assets/flags/SI.svg'
+import SO from 'assets/flags/SO.svg'
+import ZA from 'assets/flags/ZA.svg'
+import SS from 'assets/flags/SS.svg'
+import ES from 'assets/flags/ES.svg'
+import LK from 'assets/flags/LK.svg'
+import SD from 'assets/flags/SD.svg'
+import SR from 'assets/flags/SR.svg'
+import SE from 'assets/flags/SE.svg'
+import CH from 'assets/flags/SE.svg'
+import TW from 'assets/flags/TW.svg'
+import TJ from 'assets/flags/TJ.svg'
+import TZ from 'assets/flags/TZ.svg'
+import TH from 'assets/flags/TH.svg'
+import TG from 'assets/flags/TG.svg'
+import TK from 'assets/flags/TK.svg'
+import TO from 'assets/flags/TO.svg'
+import TT from 'assets/flags/TT.svg'
+import TN from 'assets/flags/TN.svg'
+import TR from 'assets/flags/TR.svg'
+import TM from 'assets/flags/TM.svg'
+import TV from 'assets/flags/TV.svg'
+import UG from 'assets/flags/UG.svg'
+import UA from 'assets/flags/UA.svg'
+import AE from 'assets/flags/AE.svg'
+import GB from 'assets/flags/GB.svg'
+import US from 'assets/flags/US.svg'
+import UY from 'assets/flags/UY.svg'
+import UZ from 'assets/flags/UZ.svg'
+import VU from 'assets/flags/VU.svg'
+import VE from 'assets/flags/VE.svg'
+import VN from 'assets/flags/VN.svg'
+import EH from 'assets/flags/EH.svg'
+import YE from 'assets/flags/YE.svg'
+import ZM from 'assets/flags/ZM.svg'
+import ZW from 'assets/flags/ZW.svg'
+import AX from 'assets/flags/AX.svg'
+
+export {
+  EARTH,
+  CN,
+  AF,
+  AL,
+  DZ,
+  AS,
+  AD,
+  AO,
+  AI,
+  AG,
+  AR,
+  AM,
+  AW,
+  AU,
+  AT,
+  AZ,
+  BS,
+  BH,
+  BD,
+  BB,
+  BY,
+  BE,
+  BZ,
+  BJ,
+  BM,
+  BT,
+  BO,
+  BQ,
+  BA,
+  BW,
+  BR,
+  IO,
+  BG,
+  BF,
+  BI,
+  KH,
+  CM,
+  CA,
+  KY,
+  CF,
+  TD,
+  CL,
+  CX,
+  CO,
+  KM,
+  CG,
+  CR,
+  HR,
+  CU,
+  CW,
+  CY,
+  CZ,
+  DK,
+  DJ,
+  DM,
+  DO,
+  EC,
+  EG,
+  SV,
+  GQ,
+  ER,
+  EE,
+  ET,
+  FJ,
+  FI,
+  FR,
+  GA,
+  GM,
+  GE,
+  DE,
+  GH,
+  GI,
+  GR,
+  GL,
+  GD,
+  GU,
+  GT,
+  GG,
+  GN,
+  GW,
+  HT,
+  HN,
+  HK,
+  HU,
+  IS,
+  IN,
+  ID,
+  IR,
+  IQ,
+  IE,
+  IM,
+  IL,
+  IT,
+  JM,
+  JP,
+  JE,
+  JQ,
+  KZ,
+  KE,
+  KI,
+  KP,
+  KR,
+  KW,
+  KG,
+  LV,
+  LB,
+  LS,
+  LR,
+  LY,
+  LI,
+  LT,
+  LU,
+  MO,
+  MG,
+  MW,
+  MY,
+  MV,
+  ML,
+  MT,
+  MQ,
+  MR,
+  MU,
+  MX,
+  MD,
+  MC,
+  MN,
+  ME,
+  MS,
+  MA,
+  MZ,
+  MM,
+  NA,
+  NR,
+  NP,
+  NL,
+  NZ,
+  NI,
+  NE,
+  NG,
+  NU,
+  NF,
+  MP,
+  NO,
+  OM,
+  PK,
+  PW,
+  PA,
+  PG,
+  PY,
+  PE,
+  PH,
+  PN,
+  PL,
+  PT,
+  PR,
+  QA,
+  MK,
+  RO,
+  RU,
+  RW,
+  KN,
+  LC,
+  VC,
+  ST,
+  SN,
+  SL,
+  SK,
+  SI,
+  SO,
+  ZA,
+  SS,
+  ES,
+  LK,
+  SD,
+  SR,
+  SE,
+  CH,
+  TW,
+  TJ,
+  TZ,
+  TH,
+  TG,
+  TK,
+  TO,
+  TT,
+  TN,
+  TR,
+  TM,
+  TV,
+  UG,
+  UA,
+  AE,
+  GB,
+  US,
+  UY,
+  UZ,
+  VU,
+  VE,
+  VN,
+  EH,
+  YE,
+  ZM,
+  ZW,
+  AX,
+}
