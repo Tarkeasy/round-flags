663a7d9a4eb86d402d21617ea81338cb89de81d2
Taras Kulchytskyi <kulchytskyitaras@gmail.com>
2021-01-02 19:31:44 +0200

    fix: fixed MU, MD export flags
    fix: change export all flags

--- index.js
+++ index.js
@@ -1,417 +1,207 @@
-import EARTH from './flags/EARTH.svg'
-import AF from './flags/AF.svg'
-import AL from './flags/AL.svg'
-import DZ from './flags/DZ.svg'
-import AS from './flags/AS.svg'
-import AD from './flags/AD.svg'
-import AO from './flags/AO.svg'
-import AI from './flags/AI.svg'
-import AG from './flags/AG.svg'
-import AR from './flags/AR.svg'
-import AM from './flags/AM.svg'
-import AW from './flags/AW.svg'
-import AU from './flags/AU.svg'
-import AT from './flags/AT.svg'
-import AZ from './flags/AZ.svg'
-import BS from './flags/BS.svg'
-import BH from './flags/BH.svg'
-import BD from './flags/BD.svg'
-import BB from './flags/BB.svg'
-import BY from './flags/BY.svg'
-import BE from './flags/BE.svg'
-import BZ from './flags/BZ.svg'
-import BJ from './flags/BJ.svg'
-import BM from './flags/BM.svg'
-import BT from './flags/BT.svg'
-import BO from './flags/BO.svg'
-import BQ from './flags/BQ.svg'
-import BA from './flags/BA.svg'
-import BW from './flags/BW.svg'
-import BR from './flags/BR.svg'
-import IO from './flags/IO.svg'
-import BG from './flags/BG.svg'
-import BF from './flags/BF.svg'
-import BI from './flags/BI.svg'
-import KH from './flags/KH.svg'
-import CN from './flags/CN.svg'
-import CM from './flags/CM.svg'
-import CA from './flags/CA.svg'
-import KY from './flags/KY.svg'
-import CF from './flags/CF.svg'
-import TD from './flags/TD.svg'
-import CL from './flags/CL.svg'
-import CX from './flags/CX.svg'
-import CO from './flags/CO.svg'
-import KM from './flags/KM.svg'
-import CG from './flags/CG.svg'
-import CR from './flags/CR.svg'
-import HR from './flags/HR.svg'
-import CU from './flags/CU.svg'
-import CW from './flags/CW.svg'
-import CY from './flags/CY.svg'
-import CZ from './flags/CZ.svg'
-import DK from './flags/DK.svg'
-import DJ from './flags/DJ.svg'
-import DM from './flags/DM.svg'
-import DO from './flags/DO.svg'
-import EC from './flags/EC.svg'
-import EG from './flags/EG.svg'
-import SV from './flags/SV.svg'
-import GQ from './flags/GQ.svg'
-import ER from './flags/ER.svg'
-import EE from './flags/EE.svg'
-import ET from './flags/ET.svg'
-import FJ from './flags/FJ.svg'
-import FI from './flags/FI.svg'
-import FR from './flags/FR.svg'
-import GA from './flags/GA.svg'
-import GM from './flags/GM.svg'
-import GE from './flags/GE.svg'
-import DE from './flags/DE.svg'
-import GH from './flags/GH.svg'
-import GI from './flags/GI.svg'
-import GR from './flags/GR.svg'
-import GL from './flags/GL.svg'
-import GD from './flags/GD.svg'
-import GU from './flags/GU.svg'
-import GT from './flags/GT.svg'
-import GG from './flags/GG.svg'
-import GN from './flags/GN.svg'
-import GW from './flags/GW.svg'
-import HT from './flags/HT.svg'
-import HN from './flags/HN.svg'
-import HK from './flags/HK.svg'
-import HU from './flags/HU.svg'
-import IS from './flags/IS.svg'
-import IN from './flags/IN.svg'
-import ID from './flags/ID.svg'
-import IR from './flags/IR.svg'
-import IQ from './flags/IQ.svg'
-import IE from './flags/IE.svg'
-import IM from './flags/IM.svg'
-import IL from './flags/IL.svg'
-import IT from './flags/IT.svg'
-import JM from './flags/JM.svg'
-import JP from './flags/JP.svg'
-import JE from './flags/JE.svg'
-import JO from './flags/JO.svg'
-import KZ from './flags/KZ.svg'
-import KE from './flags/KE.svg'
-import KI from './flags/KI.svg'
-import KP from './flags/KP.svg'
-import KR from './flags/KR.svg'
-import KW from './flags/KW.svg'
-import KG from './flags/KG.svg'
-import LV from './flags/LV.svg'
-import LB from './flags/LB.svg'
-import LS from './flags/LS.svg'
-import LR from './flags/LR.svg'
-import LY from './flags/LY.svg'
-import LI from './flags/LI.svg'
-import LT from './flags/LI.svg'
-import LU from './flags/LU.svg'
-import MO from './flags/MO.svg'
-import MG from './flags/MG.svg'
-import MW from './flags/MW.svg'
-import MY from './flags/MY.svg'
-import MV from './flags/MV.svg'
-import ML from './flags/ML.svg'
-import MT from './flags/MT.svg'
-import MQ from './flags/MQ.svg'
-import MR from './flags/MR.svg'
-import MU from './flags/MR.svg'
-import MX from './flags/MX.svg'
-import MD from './flags/MX.svg'
-import MC from './flags/MC.svg'
-import MN from './flags/MN.svg'
-import ME from './flags/ME.svg'
-import MS from './flags/MS.svg'
-import MA from './flags/MA.svg'
-import MZ from './flags/MZ.svg'
-import MM from './flags/MM.svg'
-import NA from './flags/NA.svg'
-import NR from './flags/NR.svg'
-import NP from './flags/NP.svg'
-import NL from './flags/NL.svg'
-import NZ from './flags/NZ.svg'
-import NI from './flags/NI.svg'
-import NE from './flags/NE.svg'
-import NG from './flags/NG.svg'
-import NU from './flags/NU.svg'
-import NF from './flags/NF.svg'
-import MP from './flags/MP.svg'
-import NO from './flags/NO.svg'
-import OM from './flags/OM.svg'
-import PK from './flags/PK.svg'
-import PW from './flags/PW.svg'
-import PA from './flags/PA.svg'
-import PG from './flags/PG.svg'
-import PY from './flags/PY.svg'
-import PE from './flags/PE.svg'
-import PH from './flags/PH.svg'
-import PN from './flags/PN.svg'
-import PL from './flags/PL.svg'
-import PT from './flags/PT.svg'
-import PR from './flags/PR.svg'
-import QA from './flags/QA.svg'
-import MK from './flags/MK.svg'
-import RO from './flags/RO.svg'
-import RU from './flags/RU.svg'
-import RW from './flags/RW.svg'
-import KN from './flags/KN.svg'
-import LC from './flags/LC.svg'
-import VC from './flags/VC.svg'
-import ST from './flags/ST.svg'
-import SN from './flags/SN.svg'
-import SL from './flags/SL.svg'
-import SK from './flags/SK.svg'
-import SI from './flags/SI.svg'
-import SO from './flags/SO.svg'
-import ZA from './flags/ZA.svg'
-import SS from './flags/SS.svg'
-import ES from './flags/ES.svg'
-import LK from './flags/LK.svg'
-import SD from './flags/SD.svg'
-import SR from './flags/SR.svg'
-import SE from './flags/SE.svg'
-import CH from './flags/CH.svg'
-import TW from './flags/TW.svg'
-import TJ from './flags/TJ.svg'
-import TZ from './flags/TZ.svg'
-import TH from './flags/TH.svg'
-import TG from './flags/TG.svg'
-import TK from './flags/TK.svg'
-import TO from './flags/TO.svg'
-import TT from './flags/TT.svg'
-import TN from './flags/TN.svg'
-import TR from './flags/TR.svg'
-import TM from './flags/TM.svg'
-import TV from './flags/TV.svg'
-import UG from './flags/UG.svg'
-import UA from './flags/UA.svg'
-import AE from './flags/AE.svg'
-import GB from './flags/GB.svg'
-import US from './flags/US.svg'
-import UY from './flags/UY.svg'
-import UZ from './flags/UZ.svg'
-import VU from './flags/VU.svg'
-import VE from './flags/VE.svg'
-import VN from './flags/VN.svg'
-import EH from './flags/EH.svg'
-import YE from './flags/YE.svg'
-import ZM from './flags/ZM.svg'
-import ZW from './flags/ZW.svg'
-import AX from './flags/AX.svg'
-import SA from './flags/SA.svg'
-import SG from './flags/SG.svg'
-import RS from './flags/RS.svg'
-
-export {
-  EARTH,
-  CN,
-  AF,
-  AL,
-  DZ,
-  AS,
-  AD,
-  AO,
-  AI,
-  AG,
-  AR,
-  AM,
-  AW,
-  AU,
-  AT,
-  AZ,
-  BS,
-  BH,
-  BD,
-  BB,
-  BY,
-  BE,
-  BZ,
-  BJ,
-  BM,
-  BT,
-  BO,
-  BQ,
-  BA,
-  BW,
-  BR,
-  IO,
-  BG,
-  BF,
-  BI,
-  KH,
-  CM,
-  CA,
-  KY,
-  CF,
-  TD,
-  CL,
-  CX,
-  CO,
-  KM,
-  CG,
-  CR,
-  HR,
-  CU,
-  CW,
-  CY,
-  CZ,
-  DK,
-  DJ,
-  DM,
-  DO,
-  EC,
-  EG,
-  SV,
-  GQ,
-  ER,
-  EE,
-  ET,
-  FJ,
-  FI,
-  FR,
-  GA,
-  GM,
-  GE,
-  DE,
-  GH,
-  GI,
-  GR,
-  GL,
-  GD,
-  GU,
-  GT,
-  GG,
-  GN,
-  GW,
-  HT,
-  HN,
-  HK,
-  HU,
-  IS,
-  IN,
-  ID,
-  IR,
-  IQ,
-  IE,
-  IM,
-  IL,
-  IT,
-  JM,
-  JP,
-  JE,
-  JO,
-  KZ,
-  KE,
-  KI,
-  KP,
-  KR,
-  KW,
-  KG,
-  LV,
-  LB,
-  LS,
-  LR,
-  LY,
-  LI,
-  LT,
-  LU,
-  MO,
-  MG,
-  MW,
-  MY,
-  MV,
-  ML,
-  MT,
-  MQ,
-  MR,
-  MU,
-  MX,
-  MD,
-  MC,
-  MN,
-  ME,
-  MS,
-  MA,
-  MZ,
-  MM,
-  NA,
-  NR,
-  NP,
-  NL,
-  NZ,
-  NI,
-  NE,
-  NG,
-  NU,
-  NF,
-  MP,
-  NO,
-  OM,
-  PK,
-  PW,
-  PA,
-  PG,
-  PY,
-  PE,
-  PH,
-  PN,
-  PL,
-  PT,
-  PR,
-  QA,
-  MK,
-  RO,
-  RU,
-  RW,
-  KN,
-  LC,
-  VC,
-  ST,
-  SN,
-  SL,
-  SK,
-  SI,
-  SO,
-  ZA,
-  SS,
-  ES,
-  LK,
-  SD,
-  SR,
-  SE,
-  CH,
-  TW,
-  TJ,
-  TZ,
-  TH,
-  TG,
-  TK,
-  TO,
-  TT,
-  TN,
-  TR,
-  TM,
-  TV,
-  UG,
-  UA,
-  AE,
-  GB,
-  US,
-  UY,
-  UZ,
-  VU,
-  VE,
-  VN,
-  EH,
-  YE,
-  ZM,
-  ZW,
-  AX,
-  SA,
-  SG,
-  RS,
-}
+export { default as EARTH } from './flags/EARTH.svg'
+export { default as AF } from './flags/AF.svg'
+export { default as AL } from './flags/AL.svg'
+export { default as DZ } from './flags/DZ.svg'
+export { default as AS } from './flags/AS.svg'
+export { default as AD } from './flags/AD.svg'
+export { default as AO } from './flags/AO.svg'
+export { default as AI } from './flags/AI.svg'
+export { default as AG } from './flags/AG.svg'
+export { default as AR } from './flags/AR.svg'
+export { default as AM } from './flags/AM.svg'
+export { default as AW } from './flags/AW.svg'
+export { default as AU } from './flags/AU.svg'
+export { default as AT } from './flags/AT.svg'
+export { default as AZ } from './flags/AZ.svg'
+export { default as BS } from './flags/BS.svg'
+export { default as BH } from './flags/BH.svg'
+export { default as BD } from './flags/BD.svg'
+export { default as BB } from './flags/BB.svg'
+export { default as BY } from './flags/BY.svg'
+export { default as BE } from './flags/BE.svg'
+export { default as BZ } from './flags/BZ.svg'
+export { default as BJ } from './flags/BJ.svg'
+export { default as BM } from './flags/BM.svg'
+export { default as BT } from './flags/BT.svg'
+export { default as BO } from './flags/BO.svg'
+export { default as BQ } from './flags/BQ.svg'
+export { default as BA } from './flags/BA.svg'
+export { default as BW } from './flags/BW.svg'
+export { default as BR } from './flags/BR.svg'
+export { default as IO } from './flags/IO.svg'
+export { default as BG } from './flags/BG.svg'
+export { default as BF } from './flags/BF.svg'
+export { default as BI } from './flags/BI.svg'
+export { default as KH } from './flags/KH.svg'
+export { default as CN } from './flags/CN.svg'
+export { default as CM } from './flags/CM.svg'
+export { default as CA } from './flags/CA.svg'
+export { default as KY } from './flags/KY.svg'
+export { default as CF } from './flags/CF.svg'
+export { default as TD } from './flags/TD.svg'
+export { default as CL } from './flags/CL.svg'
+export { default as CX } from './flags/CX.svg'
+export { default as CO } from './flags/CO.svg'
+export { default as KM } from './flags/KM.svg'
+export { default as CG } from './flags/CG.svg'
+export { default as CR } from './flags/CR.svg'
+export { default as HR } from './flags/HR.svg'
+export { default as CU } from './flags/CU.svg'
+export { default as CW } from './flags/CW.svg'
+export { default as CY } from './flags/CY.svg'
+export { default as CZ } from './flags/CZ.svg'
+export { default as DK } from './flags/DK.svg'
+export { default as DJ } from './flags/DJ.svg'
+export { default as DM } from './flags/DM.svg'
+export { default as DO } from './flags/DO.svg'
+export { default as EC } from './flags/EC.svg'
+export { default as EG } from './flags/EG.svg'
+export { default as SV } from './flags/SV.svg'
+export { default as GQ } from './flags/GQ.svg'
+export { default as ER } from './flags/ER.svg'
+export { default as EE } from './flags/EE.svg'
+export { default as ET } from './flags/ET.svg'
+export { default as FJ } from './flags/FJ.svg'
+export { default as FI } from './flags/FI.svg'
+export { default as FR } from './flags/FR.svg'
+export { default as GA } from './flags/GA.svg'
+export { default as GM } from './flags/GM.svg'
+export { default as GE } from './flags/GE.svg'
+export { default as DE } from './flags/DE.svg'
+export { default as GH } from './flags/GH.svg'
+export { default as GI } from './flags/GI.svg'
+export { default as GR } from './flags/GR.svg'
+export { default as GL } from './flags/GL.svg'
+export { default as GD } from './flags/GD.svg'
+export { default as GU } from './flags/GU.svg'
+export { default as GT } from './flags/GT.svg'
+export { default as GG } from './flags/GG.svg'
+export { default as GN } from './flags/GN.svg'
+export { default as GW } from './flags/GW.svg'
+export { default as HT } from './flags/HT.svg'
+export { default as HN } from './flags/HN.svg'
+export { default as HK } from './flags/HK.svg'
+export { default as HU } from './flags/HU.svg'
+export { default as IS } from './flags/IS.svg'
+export { default as IN } from './flags/IN.svg'
+export { default as ID } from './flags/ID.svg'
+export { default as IR } from './flags/IR.svg'
+export { default as IQ } from './flags/IQ.svg'
+export { default as IE } from './flags/IE.svg'
+export { default as IM } from './flags/IM.svg'
+export { default as IL } from './flags/IL.svg'
+export { default as IT } from './flags/IT.svg'
+export { default as JM } from './flags/JM.svg'
+export { default as JP } from './flags/JP.svg'
+export { default as JE } from './flags/JE.svg'
+export { default as JO } from './flags/JO.svg'
+export { default as KZ } from './flags/KZ.svg'
+export { default as KE } from './flags/KE.svg'
+export { default as KI } from './flags/KI.svg'
+export { default as KP } from './flags/KP.svg'
+export { default as KR } from './flags/KR.svg'
+export { default as KW } from './flags/KW.svg'
+export { default as KG } from './flags/KG.svg'
+export { default as LV } from './flags/LV.svg'
+export { default as LB } from './flags/LB.svg'
+export { default as LS } from './flags/LS.svg'
+export { default as LR } from './flags/LR.svg'
+export { default as LY } from './flags/LY.svg'
+export { default as LI } from './flags/LI.svg'
+export { default as LT } from './flags/LT.svg'
+export { default as LU } from './flags/LU.svg'
+export { default as MO } from './flags/MO.svg'
+export { default as MG } from './flags/MG.svg'
+export { default as MW } from './flags/MW.svg'
+export { default as MY } from './flags/MY.svg'
+export { default as MV } from './flags/MV.svg'
+export { default as ML } from './flags/ML.svg'
+export { default as MT } from './flags/MT.svg'
+export { default as MQ } from './flags/MQ.svg'
+export { default as MR } from './flags/MR.svg'
+export { default as MU } from './flags/MU.svg'
+export { default as MX } from './flags/MX.svg'
+export { default as MD } from './flags/MD.svg'
+export { default as MC } from './flags/MC.svg'
+export { default as MN } from './flags/MN.svg'
+export { default as ME } from './flags/ME.svg'
+export { default as MS } from './flags/MS.svg'
+export { default as MA } from './flags/MA.svg'
+export { default as MZ } from './flags/MZ.svg'
+export { default as MM } from './flags/MM.svg'
+export { default as NA } from './flags/NA.svg'
+export { default as NR } from './flags/NR.svg'
+export { default as NP } from './flags/NP.svg'
+export { default as NL } from './flags/NL.svg'
+export { default as NZ } from './flags/NZ.svg'
+export { default as NI } from './flags/NI.svg'
+export { default as NE } from './flags/NE.svg'
+export { default as NG } from './flags/NG.svg'
+export { default as NU } from './flags/NU.svg'
+export { default as NF } from './flags/NF.svg'
+export { default as MP } from './flags/MP.svg'
+export { default as NO } from './flags/NO.svg'
+export { default as OM } from './flags/OM.svg'
+export { default as PK } from './flags/PK.svg'
+export { default as PW } from './flags/PW.svg'
+export { default as PA } from './flags/PA.svg'
+export { default as PG } from './flags/PG.svg'
+export { default as PY } from './flags/PY.svg'
+export { default as PE } from './flags/PE.svg'
+export { default as PH } from './flags/PH.svg'
+export { default as PN } from './flags/PN.svg'
+export { default as PL } from './flags/PL.svg'
+export { default as PT } from './flags/PT.svg'
+export { default as PR } from './flags/PR.svg'
+export { default as QA } from './flags/QA.svg'
+export { default as MK } from './flags/MK.svg'
+export { default as RO } from './flags/RO.svg'
+export { default as RU } from './flags/RU.svg'
+export { default as RW } from './flags/RW.svg'
+export { default as KN } from './flags/KN.svg'
+export { default as LC } from './flags/LC.svg'
+export { default as VC } from './flags/VC.svg'
+export { default as ST } from './flags/ST.svg'
+export { default as SN } from './flags/SN.svg'
+export { default as SL } from './flags/SL.svg'
+export { default as SK } from './flags/SK.svg'
+export { default as SI } from './flags/SI.svg'
+export { default as SO } from './flags/SO.svg'
+export { default as ZA } from './flags/ZA.svg'
+export { default as SS } from './flags/SS.svg'
+export { default as ES } from './flags/ES.svg'
+export { default as LK } from './flags/LK.svg'
+export { default as SD } from './flags/SD.svg'
+export { default as SR } from './flags/SR.svg'
+export { default as SE } from './flags/SE.svg'
+export { default as CH } from './flags/CH.svg'
+export { default as TW } from './flags/TW.svg'
+export { default as TJ } from './flags/TJ.svg'
+export { default as TZ } from './flags/TZ.svg'
+export { default as TH } from './flags/TH.svg'
+export { default as TG } from './flags/TG.svg'
+export { default as TK } from './flags/TK.svg'
+export { default as TO } from './flags/TO.svg'
+export { default as TT } from './flags/TT.svg'
+export { default as TN } from './flags/TN.svg'
+export { default as TR } from './flags/TR.svg'
+export { default as TM } from './flags/TM.svg'
+export { default as TV } from './flags/TV.svg'
+export { default as UG } from './flags/UG.svg'
+export { default as UA } from './flags/UA.svg'
+export { default as AE } from './flags/AE.svg'
+export { default as GB } from './flags/GB.svg'
+export { default as US } from './flags/US.svg'
+export { default as UY } from './flags/UY.svg'
+export { default as UZ } from './flags/UZ.svg'
+export { default as VU } from './flags/VU.svg'
+export { default as VE } from './flags/VE.svg'
+export { default as VN } from './flags/VN.svg'
+export { default as EH } from './flags/EH.svg'
+export { default as YE } from './flags/YE.svg'
+export { default as ZM } from './flags/ZM.svg'
+export { default as ZW } from './flags/ZW.svg'
+export { default as AX } from './flags/AX.svg'
+export { default as SA } from './flags/SA.svg'
+export { default as SG } from './flags/SG.svg'
+export { default as RS } from './flags/RS.svg'
